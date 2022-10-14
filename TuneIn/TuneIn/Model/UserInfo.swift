e9148fd750b7ffcced3925f8f282c348610d295b
Sara Missak <sarammissak@gmail.com>
2022-10-14 15:42:06 -0400

    change user struct

--- TuneIn/TuneIn/Model/UserInfo.swift
+++ TuneIn/TuneIn/Model/UserInfo.swift
@@ -11,8 +11,9 @@ struct UserInfo: Codable, Identifiable {
   var id:               UUID
   var username:         String
   var name:             String
-  var email:            String
-  var password:         String
+//  var email:            String
+//  var password:         String
+  var spotifyID:        String 
   var profileImage:     String // assuming a URL to their image
   var sentRequests:     [UUID]
   var listOfFriends:    [UUID]
