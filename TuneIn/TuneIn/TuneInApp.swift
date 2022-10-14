60f80b5620362eda1427fab98de0912350c48bba
Sara Missak <sarammissak@gmail.com>
2022-10-14 14:01:42 -0400

    initial project from XCode

---
+++ TuneIn/TuneIn/TuneInApp.swift
@@ -0,0 +1,17 @@
+//
+//  TuneInApp.swift
+//  TuneIn
+//
+//  Created by Sara Missak on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TuneInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
