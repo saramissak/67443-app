da42e37554b37cc12a82abc9f87076447b25d4e6
Sara Missak <sarammissak@gmail.com>
2022-10-24 18:16:07 -0400

    Fix getting response from search request playground

--- Spotify-api-tests.playground/Contents.swift
+++ Spotify-api-tests.playground/Contents.swift
@@ -1,17 +1,18 @@
 import UIKit
 import Foundation
 
+// here are the structs we are using in our app
 struct Song: Codable {
   var songId:      String?
   var songName:    String?
   var spotifyLink: ExternalURL?
   var artists:     [Artist]?
-  var albumURL:    [AlbumImage]? //picture of the album
+  var albumURL:    AlbumImage? //picture of the album
   var previewUrl: String?
 
   enum CodingKeys : String, CodingKey {
     case songId      = "id"
-    case albumURL    = "images"
+    case albumURL    = "album"
     case songName    = "name"
     case spotifyLink = "external_urls"
     case artists
@@ -20,9 +21,17 @@ struct Song: Codable {
 }
 
 struct AlbumImage: Codable {
-  var url: String
+  var images: [Images]?
   
   enum CodingKeys : String, CodingKey {
+    case images
+  }
+}
+
+struct Images: Codable {
+  var url: String?
+  
+  enum CodingKeys: String, CodingKey {
     case url
   }
 }
@@ -36,7 +45,7 @@ struct ExternalURL: Codable {
 }
 
 struct Artist: Codable {
-  var name: String
+  var name: String?
   var spotifyLink: ExternalURL? // gets the link to the artist's spotify URL
   
   enum CodingKeys : String, CodingKey {
@@ -45,32 +54,33 @@ struct Artist: Codable {
   }
 }
 
-//https://developer.spotify.com/documentation/web-api/reference/#/operations/search
+// https://developer.spotify.com/documentation/web-api/reference/#/operations/search
 struct SearchResponse: Codable {
-//  var tracks: Track?
+  var tracks: Track?
 }
 
 struct Track: Codable {
-  var items: [Song]? // I think will work unsure about alumn image though
+  var items: [Song]? // I think will work unsure about album image though
   var next: String?
 }
 
 // to get bearer token: https://developer.spotify.com/console/get-search-item/?q=stitches&type=track&market=&limit=&offset=&include_external=
-let bearerToken = ""
+let bearerToken = "BQCJ-iBaWDvHHB-1ItV3wmMI1gtRYGSG43hzVfi5lCnXEALYhcdkLB47zr0o2gXz4r2XOx3Mg28bvUtpuPYsTomIFkSukWYhXgZAVGiUW1yD-Dc7PqlKdbv2A0lsAudUv9wDtfWU4I0pJ93mPhu85HeTgPUeazJs7rLUIBgKrelN71SvqPk"
 
+// the search endpoint for the API
 let SpotifyURL = URL(string: "https://api.spotify.com/v1/search?q=stitches&type=track")!
 var SpotifyRequest = URLRequest(url: SpotifyURL)
 
-var sessionConfiguration = URLSessionConfiguration.default // 5
+var sessionConfiguration = URLSessionConfiguration.default
 
 sessionConfiguration.httpAdditionalHeaders = [
-    "Authorization": "Bearer \(bearerToken)" // 6
+    "Authorization": "Bearer \(bearerToken)"
 ]
 
 let session = URLSession(configuration: sessionConfiguration)
 
 let cancellable = session
-    .dataTaskPublisher(for: SpotifyRequest) //5
+    .dataTaskPublisher(for: SpotifyRequest)
     .sink(receiveCompletion: { completion in
         switch completion {
         case let .failure(reason):
@@ -79,18 +89,14 @@ let cancellable = session
             print("")
         }
     }) { receivedValue in
-//        print( //6
-//            String(data: receivedValue.data, encoding: .utf8) ?? "Unknown"
-//        )
         let decoder  = JSONDecoder()
         let resultString = String(data: receivedValue.data, encoding: .utf8) ?? "Unknown"
-//      print(resultString)
         let jsonData = Data(resultString.utf8)
       
         do {
-          let people = try decoder.decode([SearchResponse].self, from: jsonData)
-            print(people)
+          let searchResponse = try decoder.decode(SearchResponse.self, from: jsonData)
+          print(searchResponse)
         } catch {
-            print(error)
+          print(String(describing: error))
         }
     }
