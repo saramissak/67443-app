1f0e6544f93d93a3ef5d7614e4f26e763b1ef0bb
Sara Missak <sarammissak@gmail.com>
2022-10-24 18:44:05 -0400

    add get track to playground

--- Spotify-api-tests.playground/Contents.swift
+++ Spotify-api-tests.playground/Contents.swift
@@ -16,7 +16,7 @@ struct Song: Codable {
     case songName    = "name"
     case spotifyLink = "external_urls"
     case artists
-    case previewUrl  = "prevview_url"
+    case previewUrl  = "preview_url"
   }
 }
 
@@ -60,27 +60,29 @@ struct SearchResponse: Codable {
 }
 
 struct Track: Codable {
-  var items: [Song]? // I think will work unsure about album image though
+  var items: [Song]?
   var next: String?
 }
 
 // to get bearer token: https://developer.spotify.com/console/get-search-item/?q=stitches&type=track&market=&limit=&offset=&include_external=
-let bearerToken = "BQCJ-iBaWDvHHB-1ItV3wmMI1gtRYGSG43hzVfi5lCnXEALYhcdkLB47zr0o2gXz4r2XOx3Mg28bvUtpuPYsTomIFkSukWYhXgZAVGiUW1yD-Dc7PqlKdbv2A0lsAudUv9wDtfWU4I0pJ93mPhu85HeTgPUeazJs7rLUIBgKrelN71SvqPk"
-
-// the search endpoint for the API
-let SpotifyURL = URL(string: "https://api.spotify.com/v1/search?q=stitches&type=track")!
-var SpotifyRequest = URLRequest(url: SpotifyURL)
+let bearerToken = ""
 
 var sessionConfiguration = URLSessionConfiguration.default
-
 sessionConfiguration.httpAdditionalHeaders = [
     "Authorization": "Bearer \(bearerToken)"
 ]
 
 let session = URLSession(configuration: sessionConfiguration)
 
-let cancellable = session
-    .dataTaskPublisher(for: SpotifyRequest)
+// the search endpoint for the API
+// developer console: https://developer.spotify.com/console/get-search-item/
+// API documentation: https://developer.spotify.com/documentation/web-api/reference/#/operations/search
+let songToSearchFor = "stitches"
+let SpotifySearchURL = URL(string: "https://api.spotify.com/v1/search?q=\(songToSearchFor)&type=track")!
+var SpotifySearchRequest = URLRequest(url: SpotifySearchURL)
+// gets and parses the data for a search
+var callable = session
+    .dataTaskPublisher(for: SpotifySearchRequest)
     .sink(receiveCompletion: { completion in
         switch completion {
         case let .failure(reason):
@@ -94,8 +96,37 @@ let cancellable = session
         let jsonData = Data(resultString.utf8)
       
         do {
-          let searchResponse = try decoder.decode(SearchResponse.self, from: jsonData)
+          var searchResponse = try decoder.decode(SearchResponse.self, from: jsonData)
           print(searchResponse)
+        } catch {
+          print(String(describing: error))
+        }
+    }
+
+// gets and parses the data for getting a track
+// developer console: https://developer.spotify.com/console/get-track/
+// documentation: https://developer.spotify.com/documentation/web-api/reference/#/operations/get-track
+let id = "5jsw9uXEGuKyJzs0boZ1bT"
+let SpotifyGetTrackURL = URL(string: "https://api.spotify.com/v1/tracks/\(id)")!
+var SpotifyGetTrackRequest = URLRequest(url: SpotifyGetTrackURL)
+// gets and parses the data for a search
+var callable2 = session
+    .dataTaskPublisher(for: SpotifyGetTrackRequest)
+    .sink(receiveCompletion: { completion in
+        switch completion {
+        case let .failure(reason):
+            print(reason)
+        case .finished:
+            print("")
+        }
+    }) { receivedValue in
+        let decoder  = JSONDecoder()
+        let resultString = String(data: receivedValue.data, encoding: .utf8) ?? "Unknown"
+        let jsonData = Data(resultString.utf8)
+      
+        do {
+          var song = try decoder.decode(Song.self, from: jsonData)
+          print(song)
         } catch {
           print(String(describing: error))
         }
