7e8b9c7620cacb94a60cd160a0d782b2cd17eb1e
Sara Missak <sarammissak@gmail.com>
2022-10-27 13:52:43 -0400

    Make stubs for API calls

--- Spotify-api-tests.playground/Contents.swift
+++ Spotify-api-tests.playground/Contents.swift
@@ -65,60 +65,68 @@ struct Track: Codable {
 }
 
 // to get bearer token: https://developer.spotify.com/console/get-search-item/?q=stitches&type=track&market=&limit=&offset=&include_external=
-let bearerToken = "BQDThZv3Uz9i2iVmw9ItwZ3bOHoC2bOXHxieTxUFqtAMmEjhP-04NgSIv6VjCFJQQ0W5Eo2oAdu2Pisz_D3-R2j4vbrTQcYSWnOEG4A_uZcUikFm5sw00nsZ2i8BaKBECla-jDdL9cjbF7h90ggT30MT7HztDqun9S7pwXpNASJHXCJTjb8"
+let bearerToken = "BQATC1Any-PphmZgNDMHg44HWNWe9GR_69yu0usZQklbz0XbnisKhDofA0xZQR0iyxibGOfcdfid4Ncdh7xswcGjhdtaiilgbjnVGKkJkYGXzZ4CNO37Q6I6xyxGepaCoQkcVbb7y4HJyOBq2V7OgGAnOaWlU_ZvuMLrG3xjaammu4J-IGw"
 
-var sessionConfiguration = URLSessionConfiguration.default
-sessionConfiguration.httpAdditionalHeaders = [
-    "Authorization": "Bearer \(bearerToken)"
-]
+func createSession() -> URLSession {
+  var sessionConfiguration = URLSessionConfiguration.default
+  sessionConfiguration.httpAdditionalHeaders = [
+      "Authorization": "Bearer \(bearerToken)"
+  ]
 
-let session = URLSession(configuration: sessionConfiguration)
+  return URLSession(configuration: sessionConfiguration)
+}
 
-// the search endpoint for the API
+// the search endpoint for the API, This will be for when a user wants to search for a song when creating a new post
 // developer console: https://developer.spotify.com/console/get-search-item/
-// API documentation: https://developer.spotify.com/documentation/web-api/reference/#/operations/search
-let songToSearchFor = "stitches"
-let SpotifySearchURL = "https://api.spotify.com/v1/search?q=\(songToSearchFor)&type=track"
+// documentation on search API endpoint: https://developer.spotify.com/documentation/web-api/reference/#/operations/search
+func searchForSong(_ songToSearchFor: String) {
+  let session = createSession()
+  let SpotifySearchURL = "https://api.spotify.com/v1/search?q=\(songToSearchFor)&type=track"
 
-let searchTask = session.dataTask(with: URL(string: SpotifySearchURL)!) { (data, response, error) in
-  guard let data = data else {
-    print("Error: No data to decode")
-    return
+  let searchTask = session.dataTask(with: URL(string: SpotifySearchURL)!) { (data, response, error) in
+    guard let data = data else {
+      print("Error: No data to decode")
+      return
+    }
+    
+    // Decode the JSON here
+    guard let response = try? JSONDecoder().decode(SearchResponse.self, from: data) else {
+      print("Error: Couldn't decode data into a result")
+      return
+    }
+    
+    // Output if everything is working right
+    print("\(response)")
   }
-  
-  // Decode the JSON here
-  guard let response = try? JSONDecoder().decode(SearchResponse.self, from: data) else {
-    print("Error: Couldn't decode data into a result")
-    return
-  }
-  
-  
-  // Output if everything is working right
-  print("\(response)")
+
+  searchTask.resume()
 }
 
-searchTask.resume()
-
-// gets and parses the data for getting a track
+// gets and parses the data for getting a song. This will be used for retrieving previous song of the day
 // developer console: https://developer.spotify.com/console/get-track/
-// documentation: https://developer.spotify.com/documentation/web-api/reference/#/operations/get-track
-let id = "5jsw9uXEGuKyJzs0boZ1bT"
-let SpotifyGetTrackURL = "https://api.spotify.com/v1/tracks/\(id)"
-
-let getTrackTask = session.dataTask(with: URL(string: SpotifyGetTrackURL)!) { (data, response, error) in
-  guard let data = data else {
-    print("Error: No data to decode")
-    return
+// documentation on get track API endpoint: https://developer.spotify.com/documentation/web-api/reference/#/operations/get-track
+func getSongById(_ id: String) {
+  let session = createSession()
+  let SpotifyGetTrackURL = "https://api.spotify.com/v1/tracks/\(id)"
+  
+  let getTrackTask = session.dataTask(with: URL(string: SpotifyGetTrackURL)!) { (data, response, error) in
+    guard let data = data else {
+      print("Error: No data to decode")
+      return
+    }
+    
+    // Decode the JSON here
+    guard let response = try? JSONDecoder().decode(Song.self, from: data) else {
+      print("Error: Couldn't decode data into a result")
+      return
+    }
+    
+    // Output if everything is working right
+    print("\(response)")
   }
   
-  // Decode the JSON here
-  guard let response = try? JSONDecoder().decode(Song.self, from: data) else {
-    print("Error: Couldn't decode data into a result")
-    return
-  }
-  
-  // Output if everything is working right
-  print("\(response)")
+  getTrackTask.resume()
 }
 
-getTrackTask.resume()
+searchForSong("stitches")
+getSongById("5jsw9uXEGuKyJzs0boZ1bT")
