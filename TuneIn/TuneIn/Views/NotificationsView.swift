8173ca8177dc1019bc9d1bf4d4ca1982619e26e9
Luke Arney <arney.luke@gmail.com>
2022-11-07 01:56:43 -0500

    Add the views for Friends, Notifications, & Profiles
    added new files to Views folder for future UI development

---
+++ TuneIn/TuneIn/Views/NotificationsView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationsView.swift
+//  TuneIn
+//
+//  Created by Luke Arney on 11/7/22.
+//
+
+import SwiftUI
+
+struct NotificationsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NotificationsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationsView()
+    }
+}
