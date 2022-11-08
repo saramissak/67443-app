aafeec99c0d1cca4088def1d9e5c777c5915a8c4
stellay-cmu <79229844+stellay-cmu@users.noreply.github.com>
2022-11-08 04:46:59 -0500

    basic data retrieval for homefeed

--- TuneIn/TuneIn/Views/PostCard.swift
+++ TuneIn/TuneIn/Views/PostCard.swift
@@ -8,10 +8,15 @@
 import SwiftUI
 
 struct PostCard: View {
-  let post: Post
-  
+  var post: Post
     var body: some View {
-      Text("\(post.songID)")
+      Text("\(post.song.songName ?? "no song")")
+      Text("\(post.caption)")
+      Text("\(post.userID)")
+//      Text("\(post.moods)")
+      ForEach(post.moods, id:\.self){ mood in
+        roundedRectangleText(bodyText: mood, TextHex: "#000000", BackgroundHex: "#B9C0FF")
+      }
     }
 }
 
