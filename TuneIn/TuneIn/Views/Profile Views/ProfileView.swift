9596b0779b486344546d81eb7461b25b0fac0800
Luke Arney <arney.luke@gmail.com>
2022-11-16 16:06:31 -0500

    fix image in profileview
    made it so it's resizeable

--- TuneIn/TuneIn/Views/Profile Views/ProfileView.swift
+++ TuneIn/TuneIn/Views/Profile Views/ProfileView.swift
@@ -107,6 +107,7 @@ struct ProfileSongOfDay : View {
           .bold()
         HStack(alignment: .top){
           Image(uiImage: albumImage)
+            .resizable()
             .aspectRatio(contentMode: .fit)
             .frame(width: 100, height: 100)
             .clipped()
