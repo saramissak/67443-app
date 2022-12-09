7a467b2aadbda1c7e96ee35b6d62d708451a5ddb
Luke Arney <arney.luke@gmail.com>
2022-12-08 22:46:11 -0500

    Other files from pod install

--- TuneIn/TuneInTests/HomeFeedTests.swift
+++ TuneIn/TuneInTests/HomeFeedTests.swift
@@ -1,45 +1,45 @@
+////
+////  HomeFeedTests.swift
+////  TuneInTests
+////
+////  Created by The Family on 12/7/22.
+////
 //
-//  HomeFeedTests.swift
-//  TuneInTests
+//import XCTest
+//@testable import TuneIn
+//import FirebaseFirestore
+//import Firebase
+//import FirebaseDatabase
+//import FirebaseFirestoreSwift
+//import Spartan
 //
-//  Created by The Family on 12/7/22.
+//final class HomeFeedTests: XCTestCase {
+//  var viewModel: ViewModel
+//    override func setUp() throws {
+//      viewModel = ViewModel()
+//      
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
 //
-
-import XCTest
-@testable import TuneIn
-import FirebaseFirestore
-import Firebase
-import FirebaseDatabase
-import FirebaseFirestoreSwift
-import Spartan
-
-final class HomeFeedTests: XCTestCase {
-  var viewModel: ViewModel
-    override func setUp() throws {
-      viewModel = ViewModel()
-      
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testGetPosts() throws {
-      
-      
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
-}
+//    override func tearDown() throws {
+//        // Put teardown code here. This method is called after the invocation of each test method in the class.
+//    }
+//
+//    func testGetPosts() throws {
+//      
+//      
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//        // Any test you write for XCTest can be annotated as throws and async.
+//        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+//        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
+//
+//}
