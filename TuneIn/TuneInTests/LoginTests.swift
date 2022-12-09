def90d9d12b6d90c4fddc1eee911787829be8e5a
Luke Arney <arney.luke@gmail.com>
2022-12-08 22:40:21 -0500

    update podfile and loginTest

--- TuneIn/TuneInTests/LoginTests.swift
+++ TuneIn/TuneInTests/LoginTests.swift
@@ -16,21 +16,22 @@ import Spartan
 
 final class LoginTests: XCTestCase {
   
-  var viewModel: ViewModel
-    override func setUp() throws {
-      viewModel = ViewModel()
-
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+  var viewModel = ViewModel()
+//    override func setUp() throws {
+//      viewModel = ViewModel()
+//
+//        // Put setup code here. This method is called before the invocation of each test method in the class.
+//    }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testLogin() throws {
-      viewModel.login()
+      
       Spartan.authorizationToken = "BQC5gIjLy-PNwVzdNzGUTKlOEjriwutW-DEnmv-aA2MwFDASkW39AwB8f29ttWbjlDIQprh-Jo7UPJVt0pmTf_F5xmdFpScEG2VWgjKMuBqaYncF7l3x2_a0BbgOxQPwlHHmhaTnSRxQYVhdYTz6A-E6YH_VNFC3eKbljr7xb7jqNXvNKzFJo6-FHEA3w-lVgNQc83DO2d0"
-      XCAssertEqual(viewModel.spotifyID = "5t5kzptmezvdtdy40ni9xriti")
+      viewModel.login()
+      XCTAssertEqual(viewModel.spotifyID,"5t5kzptmezvdtdy40ni9xriti")
       
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
